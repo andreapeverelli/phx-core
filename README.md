411e04e3243b4dada38a32b155880ece11f9443c
Andrea Peverelli <ndrpeverelli@gmail.com>
2026-08-19 02:52:13 +0200

    Release v0.1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# phx-core
+A frontend hypermedia framework for PHP.
+
+## Install from Repository
+### Add Repository to composer.json
+```bash
+"repositories": [
+        {
+            "type": "vcs",
+            "url": "https:\/\/github.com\/andreapeverelli\/phx-core.git"
+        },
+    ]
+```
+### Install
+```bash
+composer require andreapeverelli/phx-core
+```
---
+++ src/component/Component.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+ * Copyright (c) 2026 Andrea Peverelli
+ * https://github.com/andreapeverelli/phx-core.git
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+/**
+ * @file Component.php
+ * @brief Base component implementation.
+ */
+namespace AndreaPeverelli\PhxCore;
+
+use Mustache\Engine;
+
+/**
+ * Base class for all PHX components.
+ *
+ * It manages mustache templates rendering, PHX building and properties normalization.
+ */
+abstract class Component
+{
+	/**
+	 * Registered props indexed by component_id
+	 *
+	 * @var array<string, object>
+	 */
+	public array $props = ["default" => []];
+
+	/**
+	 * Normalized HTML attributes indexed by component_id
+	 *
+	 * @var array<string, array<array{key: string, value: string}>>
+	 */
+	public array $attributes = ["default" => []];
+	public object $context;
+
+	public string $html = "";
+
+	/**
+	 * Registers props for a component in the internal state.
+	 *
+	 * @param object $props
+	 * @param null|string $component_id
+	 */
+	final protected function registerProps(object $props, ?string $component_id = null): void
+	{
+		$this->props[$component_id ?? "default"] = $props;
+	}
+
+	/**
+	 * Get a component props from the internal state.
+	 *
+	 * @param null|string $component_id
+	 */
+	final protected function getProps(?string $component_id = null): object
+	{
+		return $this->props[$component_id ?? "default"];
+	}
+
+	/**
+	 * Normalize and reutrn a component attributes.
+	 *
+	 * @param null|string $component_id
+	 */
+	final protected function getAttributes(?string $component_id = null): array
+	{
+		$this->buildAttributes(component_id: $component_id ?? "default");
+
+		return $this->attributes[$component_id ?? "default"];
+	}
+
+	/**
+	 * Normalize a component attributes based on the component props->attributes.
+	 *
+	 * @param string $component_id
+	 */
+	private function buildAttributes(string $component_id): void
+	{
+		if(isset($this->props[$component_id])) {
+			$is_id_set = false;
+			$this->attributes[$component_id] = [];
+
+			foreach($this->props[$component_id]->attributes as $key => $value) {
+				if($key === "id") $is_id_set = true;
+
+				if($key === "class")
+					array_push(
+						$this->attributes[$component_id], [
+							"key" => $key,
+							"value" => implode(" ", $value),
+						]
+					);
+				else array_push(
+					$this->attributes[$component_id],
+					["key" => $key, "value" => $value]
+				);
+			}
+
+			if(!$is_id_set) array_push(
+				$this->attributes[$component_id],
+				["key" => "id", "value" => uniqid()],
+			);
+		} else $this->attributes[$component_id] = [["key" => "id", "value" => uniqid()]];
+	}
+
+	/**
+	 * Build the component HTML, CSS and JS; provides a used fonts and color palette list too.
+	 */
+	final protected function build(): void
+	{
+		$this->render();
+	}
+
+	/**
+	 * Render a mustache template based on the provided context.
+	 */
+	private function render(): void
+	{
+		$mustache = new Engine(["entity_flags" => ENT_QUOTES]);
+
+		$this->html = $mustache->render($this->template, $this->context);
+	}
+}
